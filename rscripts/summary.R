27edbbb8440a9e8b5858f57109a1cc8c05ab3eec
Rebaz Saleh <rebaz88.software@gmail.com>
2021-05-12 20:36:10 +0300

    wip

--- rscripts/summary.R
+++ rscripts/summary.R
@@ -6,7 +6,12 @@ source('./import_data.R')
 bestCandidateFitnessAverage <- mean(allMethodsRoundsDF$`Best Candidate Fitness`)
 bestCandidateStandardDeviation <- mean(allMethodsRoundsDF$`Best Candidate Standard Deviation`)
 meanFitness <- mean(allMethodsRoundsDF$`Mean Fitness`)
-terminationByGenerationCount <- nrow(allMethodsRoundsDF[allMethodsRoundsDF$Generation == "998",])
+totalNumberOfGenerations <- nrow(allMethodsRoundsDF)
+
+df <- filter(allMethodsRoundsDF, Generation == 999)
+terminationByGenerationCount <- nrow(df)
+
+
 
 ellapsedTimeInSummaryDF <- allMethodsRoundsDF %>% 
   group_by(`Method Name`, Round) %>%
@@ -15,12 +20,11 @@ ellapsedTimeInSummaryDF <- allMethodsRoundsDF %>%
 
 ellapsedTimeInSummaryInSeconds <- sum(ellapsedTimeInSummaryDF$`Elapsed Time(Milli Second)`) / 1000
 
-attributes <- c('Best candidate fitness average', 'Best candidate standard deviation', 'Mean fitness', 'Terminated by generation count', 'Elapsed time(Seconds)')
-results <- c(bestCandidateFitnessAverage, bestCandidateStandardDeviation, meanFitness, terminationByGenerationCount, ellapsedTimeInSummaryInSeconds)
+Attributes <- c('Best candidate fitness average', 'Best candidate standard deviation', 'Mean fitness', 'Total Number Of Generations' ,'Terminated by generation count', 'Elapsed time(Seconds)')
+Results <- c(bestCandidateFitnessAverage, bestCandidateStandardDeviation, meanFitness, totalNumberOfGenerations, terminationByGenerationCount, ellapsedTimeInSummaryInSeconds)
 
-summaryResultsDataFrame <- data.frame(attributes, results)
+summaryResultsDataFrame <- data.frame(Attributes, Results)
 
 write.csv(summaryResultsDataFrame, '../results/summary.csv')
 
-
-print(xtable(summaryResultsDataFrame, type = "latex", caption = 'Summary of the data', label = 'table:summary'), file = "../results/latex/summary.tex")
+print(xtable(summaryResultsDataFrame, type = "latex", caption = 'Summary of the data', label = 'table:summary'), include.rownames=FALSE, file = "../results/latex/summary.tex")
